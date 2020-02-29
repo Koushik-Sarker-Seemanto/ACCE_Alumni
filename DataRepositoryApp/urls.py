11cfb6a5f5630e721df37de10a2a088948efc636
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2020-02-29 17:23:14 +0600

    repository backend almost complete.

--- DataRepositoryApp/urls.py
+++ DataRepositoryApp/urls.py
@@ -8,4 +8,7 @@ app_name = 'DataRepositoryApp'
 urlpatterns = [
 
     path('alumni/', views.get_all_alumni, name='get-alumni'),
+    path('session/<session>/', views.get_by_session, name='get-session'),
+    path('profile/<int:pk>/', views.get_profile_by_id, name='get-profile'),
+    path('search/', views.search_result, name='search-result')
 ]
--- DataRepositoryApp/views.py
+++ DataRepositoryApp/views.py
@@ -7,9 +7,43 @@ from DataRepositoryApp.models import Alumni
 
 
 def get_all_alumni(request):
-    all_people = Alumni.objects.all()
+    all_people = Alumni.objects.all().order_by('-session')
 
     context = {
         'all_people': all_people
     }
     return render(request, 'DataRepositoryApp/AlumniPage.html', context)
+
+
+def get_by_session(request, session):
+    all_people = Alumni.objects.filter(session=session)
+
+    context = {
+        'current_session': session,
+        'all_people': all_people
+    }
+    return render(request, 'DataRepositoryApp/BatchPage.html', context)
+
+
+def get_profile_by_id(request, pk):
+    people = Alumni.objects.get(pk=pk)
+
+    context = {
+        'people': people
+    }
+    return render(request, 'DataRepositoryApp/Profile.html', context)
+
+
+def search_result(request):
+    search_key = request.GET.get('search_field')
+    search_by_name = Alumni.objects.filter(name__icontains=search_key)
+    search_by_session = Alumni.objects.filter(session__icontains=search_key)
+    search_by_position = Alumni.objects.filter(current_position__icontains=search_key)
+    context = {
+        'search_by_name': search_by_name,
+        'search_by_session': search_by_session,
+        'search_by_position': search_by_position,
+        'search_key': search_key
+    }
+
+    return render(request, 'DataRepositoryApp/SearchResultPage.html', context)
