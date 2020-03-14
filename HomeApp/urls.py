41e91451d6dd229c61e6d7babc84967bef82f505
Koushik-Sarker-Seemanto <koushik24@student.sust.edu>
2020-03-15 00:52:02 +0600

    upcoming event page added

--- HomeApp/urls.py
+++ HomeApp/urls.py
@@ -8,4 +8,5 @@ app_name = 'HomeApp'
 urlpatterns = [
 
     path('', views.index_view, name='home'),
+    path('events/<int:pk>/', views.upcoming_events, name='upcoming-event'),
 ]
--- HomeApp/views.py
+++ HomeApp/views.py
@@ -15,3 +15,14 @@ def index_view(request):
         "events": events,
     }
     return render(request, 'HomeApp/index.html', context)
+
+
+def upcoming_events(request, pk):
+    events = Notice.objects.all().order_by('-created_at')[:5]
+    current_event = Notice.objects.get(pk=pk)
+    context = {
+        "events": events,
+        "current_event": current_event
+    }
+    return render(request, 'HomeApp/UpcomingEventPage.html', context)
+
